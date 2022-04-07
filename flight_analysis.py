ce3530ea25aaef1b2e154a1b4c5c77d765a4f4a7
gegohcomp@gmail.com <gegohcomp@gmail.com>
2022-04-08 02:03:18 +0300

    find airline with the most total number of passengers

--- flight_analysis.py
+++ flight_analysis.py
@@ -1,16 +1,18 @@
 
 
 flightsToFrankfurtCount = 0
-flightWithMostPassengeers = "The file is empty!"
-firstFlightWithPassengersLess100 = "There is no flight with passengers less than 100."
 flightWithMostPassengers = "The file is empty!"
+firstFlightWithPassengersLess100 = "There is no flight with passengers less than 100."
+airlineWithMostPassengers = "The file is empty!"
 
 def flightAnalysis():
     try:
         # declare the above variables as global
-        global flightsToFrankfurtCount, flightWithMostPassengeers, firstFlightWithPassengersLess100, flightWithMostPassengers
+        global flightsToFrankfurtCount, flightWithMostPassengeers, firstFlightWithPassengersLess100, airlineWithMostPassengers
+
         # read data from the input file
         flightFile = open("input.txt","r")
+
         allFlights = []
         for line in flightFile:
             # add each line as a tuple
@@ -28,11 +30,52 @@ def flightAnalysis():
             printOutput()
             return
 
-        # get all flights to Frankfurt
+        flightWithMostPassengersCount = 0
+        foundFirstFlightLess100 = False
+        airlineArr = []
+        uniqueAirlineArr = []
+        airlineWithMostPassengersCount = 0
+
         for item in allFlights:
-            print ("item",item)
-            if item['destination'] == "Frankfurt":
+            airline = item["airline"]
+            destination = item["destination"]
+            passengers = item["passengers"]
+            # get all flights to Frankfurt
+            if destination == "Frankfurt":
                 flightsToFrankfurtCount += 1
+
+            # flight with the most passengers
+            if passengers > flightWithMostPassengersCount:
+                flightWithMostPassengers = airline + " " + destination + " " + str(passengers)
+                flightWithMostPassengersCount = passengers 
+
+            # flight with passengers less than 100
+            if not foundFirstFlightLess100:
+                if passengers < 100:
+                    firstFlightWithPassengersLess100 = airline + " " + destination + " " + str(passengers)
+                    foundFirstFlightLess100 = True
+
+            # add airline to its own list
+            if airline not in uniqueAirlineArr:
+                airlineArr.append(
+                    {
+                        "airline": airline,
+                        "passengers": passengers
+                    }
+                )
+                uniqueAirlineArr.append(airline)
+            else:
+                for i in airlineArr:
+                    if (i["airline"] == airline):
+                        i["passengers"] += passengers
+                        break
+
+        # find airline with the most total number of passengers
+        for airline in airlineArr:
+            passengers = airline["passengers"]
+            if passengers > airlineWithMostPassengersCount:
+                airlineWithMostPassengers = airline["airline"] + " " + str(passengers)
+                airlineWithMostPassengersCount = passengers
 
         printOutput()
 
@@ -42,8 +85,8 @@ def flightAnalysis():
 
 def printOutput():
     print (flightsToFrankfurtCount)
-    print (flightWithMostPassengeers)
-    print (firstFlightWithPassengersLess100)
     print (flightWithMostPassengers)
+    print (firstFlightWithPassengersLess100)
+    print (airlineWithMostPassengers)
 
 flightAnalysis()
